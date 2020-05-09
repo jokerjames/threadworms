9823ca3861e6a20147db4656688f2d45e5353b82
jokerjames <jamesdu09@gmail.com>
2020-05-09 16:59:48 +0800

    code finished

--- threadworms_nocomments.py
+++ threadworms_nocomments.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+#! python3
+
 import random,pygame,sys,threading
 from pygame.locals import *
 
@@ -34,19 +37,19 @@ butt = -1   # worm butt
 worms_running = True
 
 class Worm(threading.Thread):
-    def __init__(self,name='worm',maxsize=None,color=None,speed=None):
+    def __init__(self, name='worm', maxsize=None, color=None, speed=None):
         threading.Thread.__init__(self)
         # super(Worm,self).__init__()
         self.name = name
         if maxsize is None:
             self.maxsize = random.randint(4,10)
-            if random(0,4) == 0:
+            if random.randint(0,4) == 0:
                 self.maxsize += random.randint(10,20)
         else:
             self.maxsize = maxsize
 
         if color is None:
-            self.color(random.randint(60,255),random.randint(60,255),random.randint(60,255))
+            self.color = (random.randint(60,255),random.randint(60,255),random.randint(60,255))
         else:
             self.color = color
 
@@ -71,7 +74,7 @@ class Worm(threading.Thread):
         while True:
             if not worms_running:
                 return
-            if random.randint(0,100) < 20:
+            if random.randint(0,100) < 20:  #在移动的每一步上，有20%的机会虫子会随机地改变方向。
                 self.direction = random.choice((up,down,left,right))
 
             grid_lock.acquire()
@@ -81,9 +84,10 @@ class Worm(threading.Thread):
 
                 if self.direction is None:
                     self.body.reverse()
-                    self.direction = self.getNewPosition()
+                    self.direction = self.getNewDirection()
                 if self.direction is not None:
                     nextx,nexty = self.getNextPosition()
+
             if self.direction is not None:
                 grid[nextx][nexty] = self.color
                 self.body.insert(0,{'x':nextx,'y':nexty})
@@ -113,7 +117,110 @@ class Worm(threading.Thread):
             assert False,'Bad value for self.direction: %s' % self.direction
         return nextx,nexty
 
+    def getNewDirection(self):
+        x = self.body[head]['x']
+        y = self.body[head]['y']
 
+        newDirection = []
+        if y - 1 not in (-1,cells_high) and grid[x][y-1] is None:
+            newDirection.append(up)
+        if y + 1 not in (-1,cells_high) and grid[x][y+1] is None:
+            newDirection.append(down)
+        if x - 1 not in (-1,cells_wide) and grid[x-1][y] is None:
+            newDirection.append(left)
+        if x + 1 not in (-1,cells_wide) and grid[x+1][y] is None:
+            newDirection.append(right)
+
+        if newDirection == []:
+            return None
+
+        return random.choice(newDirection)
+
+def main():
+    global fpsclock, displaysurf
+
+    squares = """
+...........................
+...........................
+...........................
+.H..H..EEE..L....L.....OO..
+.H..H..E....L....L....O..O.
+.HHHH..EE...L....L....O..O.
+.H..H..E....L....L....O..O.
+.H..H..EEE..LLL..LLL...OO..
+...........................
+.W.....W...OO...RRR..MM.MM.
+.W.....W..O..O..R.R..M.M.M.
+.W..W..W..O..O..RR...M.M.M.
+.W..W..W..O..O..R.R..M...M.
+..WW.WW....OO...R.R..M...M.
+...........................
+...........................
+"""
+    setGridSquares(squares)
+
+    pygame.init()
+    fpsclock = pygame.time.Clock()
+    displaysurf = pygame.display.set_mode((windowwidth,windowheight))
+    pygame.display.set_caption('Threadworms')
+
+    worms = []
+    for i in range(num_worms):
+        worms.append(Worm(name='Worm %s' % i))
+        worms[-1].start()
+    while True:
+        handleEvents()
+        drawGrid()
+
+        pygame.display.update()
+        fpsclock.tick(fps)
+
+def handleEvents():
+    global worms_running
+    for event in pygame.event.get():
+        if (event.type == QUIT) or (event.type == KEYDOWN and event.key == K_ESCAPE):
+            worms_running = False
+            pygame.quit()
+            sys.exit()
+
+def drawGrid():
+    displaysurf.fill(bgcolor)
+    for x in range(0,windowwidth,cell_size):
+        pygame.draw.line(displaysurf,grid_line_color,(x,0),(x,windowheight))
+    for y in range(0,windowheight,cell_size):
+        pygame.draw.line(displaysurf,grid_line_color,(0,y),(windowwidth,y))
+
+    grid_lock.acquire()
+    for x in range(0,cells_wide):
+        for y in range(0,cells_high):
+            if grid[x][y] is None:
+                continue
+            color = grid[x][y]
+            darkerColor = (max(color[0] - 50,0),max(color[1] - 50,0),max(color[2] - 50,0))
+            pygame.draw.rect(displaysurf,darkerColor,(x * cell_size,y*cell_size,cell_size,cell_size))
+            pygame.draw.rect(displaysurf,color,(x*cell_size+4,y*cell_size+4,cell_size-8,cell_size-8))
+    grid_lock.release()
+
+def setGridSquares(squares,color=(192,192,192)):
+    squares = squares.split('\n')
+    if squares[0] == '':
+        del squares[0]
+    if squares[-1] == '':
+        del squares[-1]
+
+    grid_lock.acquire()
+    for y in range(min(len(squares),cells_high)):
+        for x in range(min(len(squares[y]),cells_wide)):
+            if squares[y][x] == ' ':
+                grid[x][y] = None
+            elif squares[y][x] == '.':
+                pass
+            else:
+                grid[x][y] = color
+    grid_lock.release()
+
+if __name__ == '__main__':
+    main()
 
 
 
